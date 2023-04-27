d8719cbd1ade4b3cf3ffab4ddbf61d9968abb765
soeren-schwabbauer <soeren.schwabbauer@uibk.ac.at>
2023-04-27 13:47:08 +0200

    edit spendenaufruf

---
+++ contact_mail/plumber.R
@@ -0,0 +1,7 @@
+library(plumber)
+
+#* @apiTitle Email
+
+#* Send out an email message
+#* @param email return email address
+#* @param name sender name
---
+++ docs/script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function() {
+
+  document.getElementById("my-form-2").addEventListener("submit", formsubmit);
+
+  async function formsubmit(e) {
+    
+    e.preventDefault();
+    
