206cc31961b0cc90806ac71e14df5fe599085d40
soeren-schwabbauer <soeren.schwabbauer@uibk.ac.at>
2023-04-28 15:58:19 +0200

    add images

--- docs/contact_mail/plumber.R
+++
@@ -1,7 +0,0 @@
-library(plumber)
-
-#* @apiTitle Email
-
-#* Send out an email message
-#* @param email return email address
-#* @param name sender name
--- docs/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function() {
-
-  document.getElementById("my-form-2").addEventListener("submit", formsubmit);
-
-  async function formsubmit(e) {
-    
-    e.preventDefault();
-    
